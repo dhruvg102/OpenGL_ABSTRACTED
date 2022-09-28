93a76075db7e4f9ae9e1a0bbdc0b57b9e207a815
dhruvg102 <dhruv.gupta1500@gmail.com>
2022-09-28 14:44:20 +0100

    Abstracted Renderering Pipeline

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,8 +1,92 @@
 #include <iostream>
+#include <glad/glad.h>
+#include "GLFW/glfw3.h"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#include "Renderer.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "VertexBufferLayout.h"
+#include "Shader.h"
 
 int main(){
+    GLFWwindow *window;
 
-    std::cout<<"HEY"<<std::endl;
+    if(!glfwInit()){
+        std::cout<< "GLFW not intialized" <<std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(800,800, "opengl_tut", 0,0);
+    if(!window){
+        std::cout<< "Window wasnt created" <<std::endl;
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(2);
     
+    gladLoadGL();
+    // glViewport(0,0,1280,720);
+    {
+    float position[]= {
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f,
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };    unsigned int buffer;
+
+    VertexArray va;
+    VertexBuffer vb (position, 4 * 2 * sizeof(float));
+    VertexBufferLayout layout;
+    layout.Push<float>(2);
+    va.AddBuffer(vb, layout);
+
+    IndexBuffer ib(indices, 6);
+
+    Shader shader("..\\res\\shaders\\Basic.shader");
+    shader.Bind();
+    shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+    va.Unbind();
+    vb.Unbind();
+    ib.Unbind();
+    shader.Unbind();
+
+    Renderer renderer;
+
+
+    float r = 0.0f;
+    float increment = 0.05f;
+
+    while(!glfwWindowShouldClose(window)){
+        renderer.Clear();
+        
+        shader.Bind();
+        shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+
+        renderer.Draw(va, ib, shader);
+
+        if(r > 1.0f){
+            increment = -0.05f;
+        }else if(r < 0.0f){
+            increment = 0.05f;
+        }
+        r += increment;
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+    }
+    glfwTerminate();
     return 0;
 }
